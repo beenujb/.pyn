2d06981d4280c4531e865a84df7fae1eeaa8933d
beenujb <41043473+beenujb@users.noreply.github.com>
2018-12-18 08:02:04 +0530

    Update lines.py

--- lines.py
+++ lines.py
@@ -1,7 +1,7 @@
 fname = input("Enter file name: ")
-num_lines = 0
+num of lines = 0
 with open(fname, 'r') as f:
     for line in f:
-        num_lines += 1
+        num of lines += 1
 print("Number of lines:")
-print(num_lines)
+print(num of lines)
